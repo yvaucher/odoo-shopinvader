0c0b74472efb1a2eaf988dda5d10c03dde1ed777
Sébastien BEAU <sebastien.beau@akretion.com>
2018-09-08 12:04:38 +0200

    Remove module migrated or with existing PR

--- product_stock_state/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_company
-from . import sale_config_settings
-from . import product_category
-from . import product_template
-from . import product_product
-
